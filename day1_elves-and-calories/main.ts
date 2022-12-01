9275fdf5ffb04caae21487ce735039a5032e0ec4
Jonathan Parton <jon.parton@gmail.com>
2022-12-01 22:59:49 +0000

    day 1 problem

---
+++ day1_elves-and-calories/main.ts
@@ -0,0 +1,37 @@
+import { elfString } from "./elfString";
+
+export {};
+
+const main = async () => {
+  // Grab our input
+  const elfStringsArray = elfString.split("\n\n");
+
+  // Turn our big string array into a collection of objects that track some
+  // key stats and original order.
+  const elves = elfStringsArray.map((elfArray, index) => {
+    const caloriesArray = elfArray.split("\n").map((elf) => parseInt(elf));
+
+    return {
+      originalIndex: index,
+      caloriesArray,
+      caloriesCarriedSum: caloriesArray.reduce((acc, curr) => acc + curr, 0),
+    };
+  });
+
+  // Sort our array ... We still know where they were!
+  elves.sort((a, b) => a.caloriesCarriedSum - b.caloriesCarriedSum);
+
+  console.log(
+    "Calories carried by top elf:",
+    elves[elves.length - 1].caloriesCarriedSum
+  );
+
+  console.log(
+    "Calories carried by top 3 elves:",
+    elves
+      .slice(elves.length - 3)
+      .reduce((acc, curr) => acc + curr.caloriesCarriedSum, 0)
+  );
+};
+
+main();
